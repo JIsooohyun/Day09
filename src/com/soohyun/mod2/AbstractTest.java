b4fa2f6c5dc7ee321fcd3f53ba34cdd4722a41f7
Jioohyun <opop959551@gmail.com>
2019-03-28 17:47:07 +0900

    Abstract_Final_Static 03.28

---
+++ src/com/soohyun/mod2/AbstractTest.java
@@ -0,0 +1,9 @@
+package com.soohyun.mod2;
+
+public abstract class AbstractTest {
+	
+	
+	//추상메서드
+	public abstract void test();
+
+}
---
+++ src/com/soohyun/mod2/FinalTest.java
@@ -0,0 +1,14 @@
+package com.soohyun.mod2;
+
+public final class FinalTest {
+	
+	final int MAX_NUMBER;  //상수변수로 사용하면 대문자로 사용한다. 그리고 두 문자 이상일 경우 _ 으로 구문한다.
+	
+	public FinalTest() {
+		this.MAX_NUMBER = 10;
+	}
+	public final void test() {
+
+	}
+
+}
